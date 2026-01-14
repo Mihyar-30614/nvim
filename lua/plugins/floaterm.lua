4078d9f9387d77212d0de987b68e22b6f340d8ff
Mihyar Al Masalma <eng.mihyear@gmail.com>
2026-01-13 20:14:31 -0400

    Update floating temrinal

---
+++ lua/plugins/floaterm.lua
@@ -0,0 +1,9 @@
+return {
+	"nvzone/floaterm",
+	dependencies = "nvzone/volt",
+	opts = {},
+	cmd = "FloatermToggle",
+	keys = {
+		{ "<leader>tr", "<cmd>FloatermToggle<cr>", desc = "Toogle Floating Terminal" },
+	},
+}
